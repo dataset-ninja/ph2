980d8af4d854020481685eaf23201d7971859936
Ars Vladimirov <grokhael@gmail.com>
2023-09-22 06:48:57 +0000

    lic

--- LICENSE.md
+++ LICENSE.md
@@ -1,1 +1,9 @@
-ADD CUSTOM LICENSE MANUALLY
+The data included in the PH² database can be used for research and educational purposes. It is important to note that redistribution and commercial use is not allowed. All publications that make use of this dataset must cite the following paper:
+
+```
+Teresa Mendonça, Pedro M. Ferreira, Jorge Marques, Andre R. S. Marcal, Jorge Rozeira.
+PH² - A dermoscopic image database for research and benchmarking,
+35th International Conference of the IEEE Engineering in Medicine and Biology Society, July 3-7, 2013, Osaka, Japan.
+```
+
+[Source](https://www.fc.up.pt/addi/ph2%20database.html)
--- src/settings.py
+++ src/settings.py
@@ -14,7 +14,7 @@ from dataset_tools.templates import (
 # * Before uploading to instance #
 ##################################
 PROJECT_NAME: str = "PH²"
-PROJECT_NAME_FULL: str = "PH² - A dermoscopic image database for research and benchmarking"
+PROJECT_NAME_FULL: str = "PH²: A Dermoscopic Image Database for Research and Benchmarking"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
@@ -55,7 +55,7 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 
 # If you have more than the one paper, put the most relatable link as the first element of the list
 # Use dict key to specify name for a button
-PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
+PAPER: Optional[Union[str, List[str], Dict[str, str]]] = "https://ieeexplore.ieee.org/abstract/document/6610779"
 BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
 REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
 
